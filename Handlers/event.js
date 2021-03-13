05c91ea25795dc9d230ad4b43639980758040157
Anogh297 <68709171+Anogh297@users.noreply.github.com>
2021-03-14 00:12:05 +0600

    Update event.js

--- Handlers/event.js
+++ Handlers/event.js
@@ -1,24 +1,13 @@
-const { readdirSync } = require('fs'); 
+const { readdirSync } = require("fs")
 
 module.exports = (client) => {
-
-    readdirSync('../Events/').forEach(file => {
-        const events = fs.readdirSync('../Events/').filter(file => file.endsWith('.js'));
-
-        for(let file of events) {
-
-            let pull = require(`../Events/${file}`);
-            if(pull.name) {
-
-                client.events.set(pull.name, pull);
-
-            } else {
-
-                continue;
-            
-            }
-        }
-    })
-}
-
-    
+    const load = () => {    
+        const events = readdirSync(`../Events/`).filter(file => file.endsWith('.js'));
+        for (let file of events) {
+            const evt = require(`../Events/${file}`);
+            let eName = file.split('.')[0];
+            client.on(eName, evt.bind(null, client));
+          };
+        };
+        load()
+};
